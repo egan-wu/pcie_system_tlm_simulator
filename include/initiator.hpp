8f220019fc96887d493e8eb48c8ff73c657e543b
egan-wu <eganwu177@gmail.com>
2025-08-08 19:36:56 +0800

    [Feature] introduce non-blocking push feature. Implement forward and backward transport

--- include/initiator.hpp
+++ include/initiator.hpp
@@ -3,35 +3,48 @@
 #include <tlm>
 #include <tlm_utils/simple_initiator_socket.h>
 
-using namespace sc_core;
-
-struct Initiator : sc_module {
+struct Initiator : sc_core::sc_module,
+                   public tlm::tlm_bw_transport_if<>
+{
     tlm_utils::simple_initiator_socket<Initiator> socket;
 
     SC_CTOR(Initiator) : socket("socket") {
+        socket.register_nb_transport_bw(this, &Initiator::nb_transport_bw);
         SC_THREAD(process);
     }
 
     void process() {
-        tlm::tlm_generic_payload trans;
+        tlm::tlm_generic_payload* trans = new tlm::tlm_generic_payload();
         sc_time delay = SC_ZERO_TIME;
 
         uint32_t data = 0x12345678;
-        trans.set_command(tlm::TLM_WRITE_COMMAND);
-        trans.set_address(0x1000);
-        trans.set_data_ptr(reinterpret_cast<unsigned char*>(&data));
-        trans.set_data_length(sizeof(data));
-        trans.set_streaming_width(sizeof(data));
-        trans.set_byte_enable_ptr(0);
-        trans.set_dmi_allowed(false);
-        trans.set_response_status(tlm::TLM_INCOMPLETE_RESPONSE);
+        trans->set_command(tlm::TLM_WRITE_COMMAND);
+        trans->set_address(0x1000);
+        trans->set_data_ptr(reinterpret_cast<unsigned char*>(&data));
+        trans->set_data_length(sizeof(data));
+        trans->set_streaming_width(sizeof(data));
+        trans->set_byte_enable_ptr(0);
+        trans->set_dmi_allowed(false);
+        trans->set_response_status(tlm::TLM_INCOMPLETE_RESPONSE);
 
-        socket->b_transport(trans, delay);
+        tlm::tlm_phase phase = tlm::BEGIN_REQ;
 
-        if (trans.is_response_error()) {
-            SC_REPORT_ERROR("TLM-2", "Response error from b_transport");
+        std::cout << "[Initiator] Sending BEGIN_REQ at " << sc_core::sc_time_stamp() << std::endl;
+        socket->nb_transport_fw(*trans, phase, delay);
+    }
+
+    tlm::tlm_sync_enum nb_transport_bw(
+        tlm::tlm_generic_payload& trans,
+        tlm::tlm_phase& phase,
+        sc_core::sc_time& delay) override
+    {
+        if (phase == tlm::BEGIN_RESP) {
+            std::cout << "[Initiator] Received BEGIN_RESP at " << sc_core::sc_time_stamp() << std::endl;
         }
+        return tlm::TLM_COMPLETED;
+    }
 
-        std::cout << "[Initiator] Sent write, delay: " << delay << std::endl;
+    void invalidate_direct_mem_ptr(sc_dt::uint64 start_range, sc_dt::uint64 end_range) override {
+        // 不支援 DMI，可忽略
     }
 };
--- include/target.hpp
+++ include/target.hpp
@@ -2,28 +2,54 @@
 #include <systemc>
 #include <tlm>
 #include <tlm_utils/simple_target_socket.h>
+#include <tlm_utils/peq_with_cb_and_phase.h>
 #include "config.hpp"
 
-using namespace sc_core;
-
-struct Target : sc_module {
+struct Target : sc_core::sc_module,
+                public tlm::tlm_fw_transport_if<>
+{
     tlm_utils::simple_target_socket<Target> socket;
+    tlm_utils::peq_with_cb_and_phase<Target> m_peq;
     PCIeConfig& config;
 
     SC_CTOR(Target, PCIeConfig& cfg) 
-    : socket("socket"), config(cfg) 
+    : socket("socket"), 
+      config(cfg),
+      m_peq(this, &Target::peq_callback) 
     {
-        socket.register_b_transport(this, &Target::b_transport);
+        socket.register_nb_transport_fw(this, &Target::nb_transport_fw);
     }
 
-    void b_transport(tlm::tlm_generic_payload& trans, sc_time& delay) {
-        delay += config.target_delay;
+    tlm::tlm_sync_enum nb_transport_fw(
+        tlm::tlm_generic_payload& trans,
+        tlm::tlm_phase& phase,
+        sc_core::sc_time& delay) override
+    {
+        m_peq.notify(trans, phase, delay);
+        return tlm::TLM_ACCEPTED;
+    }
 
-        std::cout << "[Target] Received "
-            << (trans.get_command() == tlm::TLM_WRITE_COMMAND ? "WRITE" : "READ")
-            << " to address 0x" << std::hex << trans.get_address()
-            << ", delay: " << delay << std::endl;
+    void peq_callback(
+        tlm::tlm_generic_payload& trans,
+        const tlm::tlm_phase& phase )
+    {
+        if (phase == tlm::BEGIN_REQ) {
+            std::cout << "[Target] Received BEGIN_REQ at " << sc_core::sc_time_stamp() << std::endl;
+            sc_core::sc_time resp_delay = config.target_delay;
+            tlm::tlm_phase resp_phase = tlm::BEGIN_RESP;
+            socket->nb_transport_bw(trans, resp_phase, resp_delay);
+        }
+    }
 
-        trans.set_response_status(tlm::TLM_OK_RESPONSE);
+    void b_transport(tlm::tlm_generic_payload& trans, sc_core::sc_time& delay) override {
+        SC_REPORT_WARNING("Target", "b_transport not implemented");
+    }
+
+    bool get_direct_mem_ptr(tlm::tlm_generic_payload& trans, tlm::tlm_dmi& dmi_data) override {
+        return false;
+    }
+
+    unsigned int transport_dbg(tlm::tlm_generic_payload& trans) override {
+        return 0;
     }
 };
