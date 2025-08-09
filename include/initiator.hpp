03b5899e27680d29e04514efdcd6d6e25f460281
egan-wu <eganwu177@gmail.com>
2025-08-09 13:08:04 +0800

    [Feature] introduce addrMap into pcie-bus, and support multiple target binding

--- include/initiator.hpp
+++ include/initiator.hpp
@@ -14,23 +14,26 @@ struct Initiator : sc_core::sc_module,
     }
 
     void process() {
-        tlm::tlm_generic_payload* trans = new tlm::tlm_generic_payload();
-        sc_time delay = SC_ZERO_TIME;
+        for (uint32_t addr = 0x200; addr < 0x2000; addr+=0x1000) {
+            tlm::tlm_generic_payload* trans = new tlm::tlm_generic_payload();
+            sc_time delay = SC_ZERO_TIME;
 
-        uint32_t data = 0x12345678;
-        trans->set_command(tlm::TLM_WRITE_COMMAND);
-        trans->set_address(0x1000);
-        trans->set_data_ptr(reinterpret_cast<unsigned char*>(&data));
-        trans->set_data_length(sizeof(data));
-        trans->set_streaming_width(sizeof(data));
-        trans->set_byte_enable_ptr(0);
-        trans->set_dmi_allowed(false);
-        trans->set_response_status(tlm::TLM_INCOMPLETE_RESPONSE);
+            uint32_t data = 0x12345678;
+            trans->set_command(tlm::TLM_WRITE_COMMAND);
+            trans->set_address(addr);
+            trans->set_data_ptr(reinterpret_cast<unsigned char*>(&data));
+            trans->set_data_length(sizeof(data));
+            trans->set_streaming_width(sizeof(data));
+            trans->set_byte_enable_ptr(0);
+            trans->set_dmi_allowed(false);
+            trans->set_response_status(tlm::TLM_INCOMPLETE_RESPONSE);
 
-        tlm::tlm_phase phase = tlm::BEGIN_REQ;
+            tlm::tlm_phase phase = tlm::BEGIN_REQ;
 
-        std::cout << "[Initiator] Sending BEGIN_REQ at " << sc_core::sc_time_stamp() << std::endl;
-        socket->nb_transport_fw(*trans, phase, delay);
+            std::cout << "[Initiator] Sending BEGIN_REQ to address: " << std::hex << trans->get_address() << std::dec << " at " << sc_core::sc_time_stamp() << std::endl;
+            socket->nb_transport_fw(*trans, phase, delay);
+        }
+
     }
 
     tlm::tlm_sync_enum nb_transport_bw(
@@ -38,6 +41,8 @@ struct Initiator : sc_core::sc_module,
         tlm::tlm_phase& phase,
         sc_core::sc_time& delay) override
     {
+        (void)trans;
+        (void)delay;
         if (phase == tlm::BEGIN_RESP) {
             std::cout << "[Initiator] Received BEGIN_RESP at " << sc_core::sc_time_stamp() << std::endl;
         }
@@ -45,6 +50,8 @@ struct Initiator : sc_core::sc_module,
     }
 
     void invalidate_direct_mem_ptr(sc_dt::uint64 start_range, sc_dt::uint64 end_range) override {
-        // 不支援 DMI，可忽略
+        // not implemented
+        (void)start_range;
+        (void)end_range;
     }
 };
